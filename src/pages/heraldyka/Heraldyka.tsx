cb4f8399b442fd8bd7c0d5c5c9a4f39ad15e0845
Szymon Tondowski <stdeykun+git@gmail.com>
2024-08-09 22:30:04 +0200

    Map always should fit the screen

--- src/pages/heraldyka/Heraldyka.tsx
+++ src/pages/heraldyka/Heraldyka.tsx
@@ -1,6 +1,6 @@
 import { useState, useMemo, useCallback } from 'react';
-import SvgGmina from './SvgGmina';
-// import SvgPowiaty from './SvgPowiaty';
+// import SvgGmina from './SvgGmina';
+import SvgPowiaty from './SvgPowiaty';
 import ListItem from './ListItem';
 import './Heraldyka.scss';
 import { colorsByNames } from './constants';
@@ -19,7 +19,8 @@ const allUnits = [...gminy, ...miasta].filter(({ title }) => title !== 'Herb Pod
 
 // const minLeft = -12;
 const minTop = 0;
-const maxTop = 54.8;
+// const maxTop = 54.8;
+const maxTop = 54.9;
 const maxBottom = 48.85;
 
 const maxLeft = 14.17;
@@ -59,58 +60,47 @@ const Heraldyka = () => {
 
     // console.log(colorFilters);
 
+    const unitsWithLocation = units.filter(
+      (unit) => typeof unit?.place?.coordinates?.lon === 'number',
+    );
+
     return (
         <>
           <h1 className="text-[48px] text-center mb-20">Herby polskich gmin i miast</h1>
-          <div className="relative mb-10">
-            <SvgGmina />
-            {/* <SvgPowiaty /> */}
-            {/* <div className="max-w-[400px]">
-
-            </div> */}
+          <div className="relative mb-10 max-h-[70vh] aspect-[820_/_775] mx-auto flex justify-center items-center">
+            {/* <SvgGmina /> */}
+            <SvgPowiaty />
             <div>
-                      {/* {units.filter(({ title }) => ['Herb Opatówka', 'Herb gminy Gubin', 'Herb gminy Barciany'].includes(title)).map((unit) => { */}
-            {units.filter(
-              (unit) => typeof unit?.place?.coordinates?.lon === 'number',
-            ).map(
-                (unit) => (
-                  <span
-                    key={unit.title}
-                    className="absolute hover:z-10"
-                    style={getPostionForPlace(unit)}
-                    data-lon={unit?.place?.coordinates?.lon}
-                    data-lat={unit?.place?.coordinates?.lat}
-                    data-width={polandWidth}
-                  >
-                    <img src={unit?.imageUrl}
-                      loading="lazy"
-                      className="size-2 sm:size-3 md:size-4 lg:size-5 scale-100 hover:scale-[800%] ease-in duration-100 object-contain"
-                      title={unit.title}
-                    />
-                </span>
-              ))}
+              {unitsWithLocation.filter(
+                (unit) => typeof unit?.place?.coordinates?.lon === 'number',
+              ).map(
+                  (unit) => (
+                    <span
+                      key={unit.title}
+                      className="absolute hover:z-10"
+                      style={getPostionForPlace(unit)}
+                      data-lon={unit?.place?.coordinates?.lon}
+                      data-lat={unit?.place?.coordinates?.lat}
+                      data-width={polandWidth}
+                    >
+                      <img src={unit?.imageUrl}
+                        loading="lazy"
+                        className="size-2 sm:size-3 md:size-4 lg:size-5 scale-100 hover:scale-[800%] ease-in duration-100 object-contain"
+                        title={unit.title}
+                      />
+                  </span>
+                ))}
             </div>
-
-            {/* <span className="absolute top-[11%] left-[8.5%]">
-              <img src="images/heraldyka/gminy/herb-gminy-rewal.png" className="size-10 hover:scale-150 hover:opacity-10 object-contain" title="Herb gminy Rewal" />
-            </span>
-            <span className="absolute top-[50%] left-[40%]">
-              <img src="images/heraldyka/gminy/herb-opatowka.png" className="size-10 hover:scale-150 hover:opacity-10 object-contain" title="Herb Opatówka" />
-            </span>
-            <span className="absolute top-[47%] left-[4%]">
-            <img src="images/heraldyka/gminy/herb-gminy-gubin.jpg" loading="lazy" className="size-10 hover:scale-150 hover:opacity-10 object-contain" title="Herb gminy Gubin" />
-            </span>
-
-            <span className="absolute top-[9%] left-[68%]">
-              <img src="images/heraldyka/gminy/herb-gminy-barciany.png" loading="lazy" className="size-10 hover:scale-150 hover:opacity-10 object-contain" title="Herb gminy Barciany"></img>
-            </span> */}
-            
           </div>
-          <h2 className="mb-5">
-              Widoczne {units.length}
-          </h2>
+          <p className="mb-5 text-right text-[12px] opacity-70">
+              Zaindeksowanych <strong>{allUnits.length}</strong>
+              {' '}
+              po odfiltrowaniu <strong>{units.length}</strong>
+              {' '}
+              na mapię <strong>{unitsWithLocation.length}</strong>.
+          </p>
           <div className="flex items-center gap-5 mb-5">
-            <h3>Filters:</h3>
+            <h3>Filtry:</h3>
             {Object.entries(colorsByNames).map(([name, color]) => <button
               key={name}
               className="block size-5 rounded-[4px] shadow-md"
