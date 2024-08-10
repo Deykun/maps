cff6a5c86b4c4046004e847dd8e662b0998e8cad
Szymon Tondowski <stdeykun+git@gmail.com>
2024-08-10 14:09:54 +0200

    Subpage build fix

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -4,7 +4,7 @@ import { PATHS_DATA } from '../src/constants';
 PATHS_DATA.forEach(({ path, title, social }) => {
   let html = fs.readFileSync('./dist/index.html', 'utf-8');
 
-  html = html.replace('<title>🗺️ maps by deykun</title>', `<title>${title}<title>`);
+  html = html.replace('<title>🗺️ maps by deykun</title>', `<title>${title}</title>`);
   html = html.replace('<!-- SOCIAL -->', social);
 
   fs.writeFileSync(`./dist/${path}.html`, html);
