48025b1e4cad8a8eb2bf3c39955d0ec8be6ce3c1
Szymon Tondowski <stdeykun+git@gmail.com>
2024-08-10 14:09:06 +0200

    Social fix

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,6 @@ export const PATHS_DATA = [
   {
     path: 'heraldyka',
     title: '🛡️ Herby polskich miast i gmin - Heraldyka, mapa | Polska',
-    social: `<meta property="og:title" content="Mapa herbów polskich miast i gmin" >
-    <meta property="og:image" content="https://deykun.github.io/maps/social/herby-polskich-gmin-i-miast.png" >`,
+    social: `<meta property="og:title" content="Mapa herbów polskich miast i gmin" /><meta property="og:image" content="https://deykun.github.io/maps/social/herby-polskich-gmin-i-miast.png" />`,
   },
 ];
