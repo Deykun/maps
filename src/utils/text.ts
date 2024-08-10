fa0e0d58eb097f8aa33a028af526e609895871ce
Szymon Tondowski <stdeykun+git@gmail.com>
2024-08-10 16:40:29 +0200

    List search & setting phrase with a click

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,105 +1,105 @@
-export const removeDiacratics = (word: string, lang?: string) => {
+export const removeDiacratics = (word: string, lang?: string, special = '_') => {
     let textToReturn = word;
 
     // If lang not passed or specified
     if (!lang || lang === 'cs') {
-        textToReturn = textToReturn.replaceAll('á', '_a')
-            .replaceAll('č', '_c')
-            .replaceAll('ď', '_d')
-            .replaceAll('é', '_e')
-            .replaceAll('ě', '_e')
-            .replaceAll('í', '_i')
-            .replaceAll('ň', '_n')
-            .replaceAll('ó', '_o')
-            .replaceAll('ř', '_r')
-            .replaceAll('š', '_s')
-            .replaceAll('ť', '_t')
-            .replaceAll('ú', '_u')
-            .replaceAll('ů', '_u')
-            .replaceAll('ý', '_y')
-            .replaceAll('ž', '_z');
+        textToReturn = textToReturn.replaceAll('á', `${special}a`)
+            .replaceAll('č', `${special}c`)
+            .replaceAll('ď', `${special}d`)
+            .replaceAll('é', `${special}e`)
+            .replaceAll('ě', `${special}e`)
+            .replaceAll('í', `${special}i`)
+            .replaceAll('ň', `${special}n`)
+            .replaceAll('ó', `${special}o`)
+            .replaceAll('ř', `${special}r`)
+            .replaceAll('š', `${special}s`)
+            .replaceAll('ť', `${special}t`)
+            .replaceAll('ú', `${special}u`)
+            .replaceAll('ů', `${special}u`)
+            .replaceAll('ý', `${special}y`)
+            .replaceAll('ž', `${special}z`);
     }
 
     if (!lang || lang === 'de') {
         textToReturn = textToReturn
-            .replaceAll('ä', '_a')
-            .replaceAll('ö', '_o')
-            .replaceAll('ß', '_s')
-            .replaceAll('ü', '_u');
+            .replaceAll('ä', `${special}a`)
+            .replaceAll('ö', `${special}o`)
+            .replaceAll('ß', `${special}s`)
+            .replaceAll('ü', `${special}u`);
     }
 
     if (!lang || lang === 'de') {
         textToReturn = textToReturn
-            .replaceAll('ä', '_a')
-            .replaceAll('ö', '_o')
-            .replaceAll('ß', '_s')
-            .replaceAll('ü', '_u');
+            .replaceAll('ä', `${special}a`)
+            .replaceAll('ö', `${special}o`)
+            .replaceAll('ß', `${special}s`)
+            .replaceAll('ü', `${special}u`);
     }
 
     if (!lang || lang === 'es') {
         textToReturn = textToReturn
-            .replaceAll('á', '_a')
-            .replaceAll('é', '_e')
-            .replaceAll('í', '_i')
-            .replaceAll('ó', '_o')
-            .replaceAll('ú', '_u')
-            .replaceAll('ü', '_ü')
-            .replaceAll('ñ', '_n');
+            .replaceAll('á', `${special}a`)
+            .replaceAll('é', `${special}e`)
+            .replaceAll('í', `${special}i`)
+            .replaceAll('ó', `${special}o`)
+            .replaceAll('ú', `${special}u`)
+            .replaceAll('ü', `${special}ü`)
+            .replaceAll('ñ', `${special}n`);
     }
 
     if (!lang || lang === 'fi') {
         textToReturn = textToReturn
-            .replaceAll('å', '_a')
-            .replaceAll('ä', '_a')
-            .replaceAll('ö', '_o')
-            .replaceAll('š', '_s')
-            .replaceAll('ž', '_z');
+            .replaceAll('å', `${special}a`)
+            .replaceAll('ä', `${special}a`)
+            .replaceAll('ö', `${special}o`)
+            .replaceAll('š', `${special}s`)
+            .replaceAll('ž', `${special}z`);
     }
 
     if (!lang || lang === 'fr') {
         textToReturn = textToReturn
-            .replaceAll('à', '_a')
-            .replaceAll('â', '_a')
-            .replaceAll('æ', '_a')
-            .replaceAll('ç', '_c')
-            .replaceAll('é', '_e')
-            .replaceAll('è', '_e')
-            .replaceAll('ê', '_e')
-            .replaceAll('ë', '_e')
-            .replaceAll('î', '_i')
-            .replaceAll('ï', '_i')
-            .replaceAll('ô', '_o')
-            .replaceAll('œ', '_o')
-            .replaceAll('ù', '_u')
-            .replaceAll('û', '_u')
-            .replaceAll('ü', '_u');
+            .replaceAll('à', `${special}a`)
+            .replaceAll('â', `${special}a`)
+            .replaceAll('æ', `${special}a`)
+            .replaceAll('ç', `${special}c`)
+            .replaceAll('é', `${special}e`)
+            .replaceAll('è', `${special}e`)
+            .replaceAll('ê', `${special}e`)
+            .replaceAll('ë', `${special}e`)
+            .replaceAll('î', `${special}i`)
+            .replaceAll('ï', `${special}i`)
+            .replaceAll('ô', `${special}o`)
+            .replaceAll('œ', `${special}o`)
+            .replaceAll('ù', `${special}u`)
+            .replaceAll('û', `${special}u`)
+            .replaceAll('ü', `${special}u`);
     }
 
     if (!lang || lang === 'it') {
         textToReturn = textToReturn
-            .replaceAll('à', '_a')
-            .replaceAll('è', '_e')
-            .replaceAll('é', '_e')
-            .replaceAll('ì', '_i')
-            .replaceAll('í', '_i')
-            .replaceAll('î', '_i')
-            .replaceAll('ò', '_o')
-            .replaceAll('ó', '_o')
-            .replaceAll('ù', '_u')
-            .replaceAll('ú', '_u');
+            .replaceAll('à', `${special}a`)
+            .replaceAll('è', `${special}e`)
+            .replaceAll('é', `${special}e`)
+            .replaceAll('ì', `${special}i`)
+            .replaceAll('í', `${special}i`)
+            .replaceAll('î', `${special}i`)
+            .replaceAll('ò', `${special}o`)
+            .replaceAll('ó', `${special}o`)
+            .replaceAll('ù', `${special}u`)
+            .replaceAll('ú', `${special}u`);
     }
 
     if (!lang || lang === 'pl') {
         textToReturn = textToReturn
-            .replaceAll('ą', '_a')
-            .replaceAll('ć', '_c')
-            .replaceAll('ę', '_e')
-            .replaceAll('ł', '_l')
-            .replaceAll('ń', '_n')
-            .replaceAll('ó', '_o')
-            .replaceAll('ś', '_s')
-            .replaceAll('ź', '_z')
-            .replaceAll('ż', '_z');
+            .replaceAll('ą', `${special}a`)
+            .replaceAll('ć', `${special}c`)
+            .replaceAll('ę', `${special}e`)
+            .replaceAll('ł', `${special}l`)
+            .replaceAll('ń', `${special}n`)
+            .replaceAll('ó', `${special}o`)
+            .replaceAll('ś', `${special}s`)
+            .replaceAll('ź', `${special}z`)
+            .replaceAll('ż', `${special}z`);
     }
 
     return textToReturn;
