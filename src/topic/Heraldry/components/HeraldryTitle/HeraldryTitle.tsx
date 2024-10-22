514dea1200135ecccb32090b48f7babac3bc7f28
Szymon Tondowski <stdeykun+git@gmail.com>
2024-10-22 12:36:43 +0200

    Improvments to title rwd

--- src/topic/Heraldry/components/HeraldryTitle/HeraldryTitle.tsx
+++ src/topic/Heraldry/components/HeraldryTitle/HeraldryTitle.tsx
@@ -26,8 +26,12 @@ const HeraldryTitle = ({
   const { t } = useTranslation();
 
   if (zoomLevel > 1) {
+    if (subtitleParts.length === 0) {
+      return null;
+    }
+
     return (
-      <div className="ui-slide-from-top fixed top-0 left-0 md:left-12 top max-w-[calc(100vw_-_55px)] md:max-w-[calc(100vw_-_95px)] flex pointer-events-none">
+      <div className="ui-slide-from-top fixed top-0 left-0 z-[10] md:left-12 top max-w-[calc(100vw_-_55px)] md:max-w-[calc(100vw_-_95px)] flex pointer-events-none">
         <Space side="top" isFirst />
         <Panel className="ui-panel--rounded-b flex-row min-h-[48px] items-center px-4">
           <HeraldrySubtitle
