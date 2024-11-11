e3e3259fa15ea31e6cb7478f31adcb80a45cd6e3
Szymon Tondowski <stdeykun+git@gmail.com>
2024-11-11 17:43:08 +0100

    Animal filters added

--- src/topic/Heraldry/utils/markers/getMarkers.ts
+++ src/topic/Heraldry/utils/markers/getMarkers.ts
@@ -1,6 +1,7 @@
 import { getMarkers as getMarkersET } from './lang/et';
 import { getMarkers as getMarkersDE } from './lang/de';
 import { getMarkers as getMarkersFI } from './lang/fi';
+import { getMarkers as getMarkersNO } from './lang/no';
 import { getMarkers as getMarkersPL } from './lang/pl';
 
 export const getMarkers = ({
@@ -42,6 +43,14 @@ export const getMarkers = ({
     items = response.items;
   }
 
+  if (lang === 'no') {
+    const response = getMarkersNO({ text: rawText, title, imageHash })
+
+    types = response.types;
+    animals = response.animals;
+    items = response.items;
+  }
+
   if (lang === 'pl') {
     const response = getMarkersPL({ text: rawText, title, imageHash });
 
@@ -50,7 +59,7 @@ export const getMarkers = ({
     items = response.items;
   }
 
-  if (!animals.includes('bird') && animals.some((active) => ['crane', 'duck', 'eagle', 'falcon', 'capercaillie', 'goose', 'gull', 'grouse', 'hawk', 'heron', 'owl', 'parrot', 'peacock', 'pelican', 'pigeon', 'phoenix', 'stork', 'raven', 'rooster', 'swan', 'vulture'].includes(active))) {
+  if (!animals.includes('bird') && animals.some((active) => ['crane', 'cormorant', 'duck', 'eagle', 'falcon', 'capercaillie', 'goose', 'gull', 'grouse', 'hawk', 'heron', 'owl', 'parrot', 'peacock', 'pelican', 'pigeon', 'phoenix', 'stork', 'raven', 'rooster', 'swan', 'vulture'].includes(active))) {
     animals.push('bird');
   }
 
