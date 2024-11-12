29800a70f2406c4e7194f4237b8ac2d7aed32f2f
Szymon Tondowski <stdeykun+git@gmail.com>
2024-11-12 15:11:03 +0100

    Danish added

--- src/locales/config.ts
+++ src/locales/config.ts
@@ -1,12 +1,16 @@
+import localeDa from './da.json';
+import localeDe from './de.json';
 import localeEn from './en.json';
 import localeEt from './et.json';
-import localeDe from './de.json';
 import localeFi from './fi.json';
 import localeNo from './no.json';
 import localePl from './pl.json';
 
 // TODO: manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 export const resources = {
+  da: {
+    translation: localeDa,
+  },
   de: {
     translation: localeDe,
   },
